c50530a26b6896ada3c827e05802bc1d0787d292
Capella42 <83754482+Capella42@users.noreply.github.com>
2023-10-26 13:38:52 +0900

    plz

---
+++ settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1862066.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 153407049, 150749338, 150975775, 153407432, 153831902 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/150771783/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/150771784/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/150771782/1/ammo.js', 'preload' : true},
+];
